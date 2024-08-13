8b6402ab62b9b650b3d280c60bfdab1e04e45e51
mrlee <lkt900520@gmail.com>
2024-08-14 03:30:59 +0900

    Initial commit

---
+++ src/main/java/study/my_git/MyGitApplication.java
@@ -0,0 +1,13 @@
+package study.my_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/my_git/MyGitApplicationTests.java
@@ -0,0 +1,13 @@
+package study.my_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
